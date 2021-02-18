a1a18cf6cc99b34efc52e2a4633f4c07f47c51bf
Czosnek <martaszablowska21@gmail.com>
2021-02-18 22:52:40 +0100

    Dodana waidacja oprocz bota i emaila

--- zarejstruj.php
+++ zarejstruj.php
@@ -19,12 +19,13 @@ $imie = $_POST['imie'];
 $nazwisko = $_POST['nazwisko'];
 $login = $_POST['login'];
 $haslo = $_POST['haslo1'];
+$haslo2 = $_POST['haslo2'];
 $email = $_POST['email'];
 $sekret = "6Lf6Wy8aAAAAABbGkT-5W8FJAIxGpIbyM4IiqvX1";
 $sprawdz = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$sekret.'&response='.$_POST['g-recaptcha-response']);
 $odpowiedz = json_decode($sprawdz); 
 
-zarejestruj($polaczenie, $imie, $nazwisko, $login, $haslo, $email);
+zarejestruj($polaczenie, $imie, $nazwisko, $login, $haslo, $haslo2, $email);
 zaloguj($polaczenie, $login);
 zakoncz($polaczenie);
 
@@ -34,8 +35,8 @@ function zakoncz($polaczenie){
     exit();
 }
 
-function zarejestruj($polaczenie, $imie, $nazwisko, $login, $haslo, $email){
-    zwaliduj_dane_uzytkownika($polaczenie, $email, $login);
+function zarejestruj($polaczenie, $imie, $nazwisko, $login, $haslo, $haslo2, $email){
+    zwaliduj_dane_uzytkownika($polaczenie, $imie, $nazwisko, $email, $login, $haslo, $haslo2);
     zapisz_uzytkownika_do_bazy($polaczenie, $imie, $nazwisko, $login, $haslo, $email);
 }
 
@@ -49,23 +50,44 @@ function zapisz_uzytkownika_do_bazy($polaczenie, $imie, $nazwisko, $login, $hasl
     }   
 }
 
-function zwaliduj_dane_uzytkownika($polaczenie, $email, $login){
+function zwaliduj_dane_uzytkownika($polaczenie, $imie, $nazwisko, $email, $login, $haslo, $haslo2){
     $mail_zajety = sprawdz_czy_mail_zajety($polaczenie, $email);
     $login_zajety = sprawdz_czy_login_zajety($polaczenie, $login);
+    $imie_niepoprawne = sprawdz_czy_imie_niepoprawne($imie);
+    $nazwisko_niepoprawne = sprawdz_czy_nazwisko_niepoprawne($nazwisko);
+    $haslo_rozne = sprawdz_czy_haslo_rozne($haslo, $haslo2);
+    $haslo_niepoprawne = sprawdz_czy_haslo_niepoprawne($haslo);
 
-    if($mail_zajety || $login_zajety){
-        wyswietl_blad_danych($mail_zajety, $login_zajety, $polaczenie);
+    if($mail_zajety || $login_zajety || $nazwisko_niepoprawne || $imie_niepoprawne || $haslo_rozne || $haslo_niepoprawne){
+        wyswietl_blad_danych($imie_niepoprawne, $nazwisko_niepoprawne, $mail_zajety, $login_zajety, $haslo_rozne, $haslo_niepoprawne, $polaczenie);
     }
 }
 
-function wyswietl_blad_danych($mail_zajety, $login_zajety, $polaczenie){
+function wyswietl_blad_danych($imie_niepoprawne, $nazwisko_niepoprawne, $mail_zajety, $login_zajety, $haslo_rozne, $haslo_niepoprawne, $polaczenie){
     $komunikat = "";
+
+    if($imie_niepoprawne){
+        $komunikat = $komunikat."Imie musi zawierać od 2 do 20 liter. ";
+    }
+
+    if($nazwisko_niepoprawne){
+        $komunikat = $komunikat."Nazwisko musi zawierać od 2 do 20 liter. ";
+    }
+
     if($mail_zajety){
         $komunikat = $komunikat."Mail zajety. ";
     }
 
     if($login_zajety){
         $komunikat = $komunikat."Login zajety.";
+    }
+
+    if($haslo_rozne){
+        $komunikat = $komunikat."Hasła różnią się od siebie. ";
+    }
+
+    if($haslo_niepoprawne){
+        $komunikat = $komunikat."Hasło musi zawierać od 8 do 20 znaków. ";
     }
 
     blad($polaczenie, $komunikat);
@@ -96,6 +118,35 @@ function sprawdz_czy_mail_zajety($polaczenie, $email){
     if($ile_takich_maili > 0)
     {
         return true;       
+    }
+    return false;
+}
+
+function sprawdz_czy_imie_niepoprawne($imie){
+    if((strlen($imie)<2) || (strlen($imie)>20) || (ctype_alpha($imie)==false)){
+        return true;
+    }
+    return false;
+}
+
+function sprawdz_czy_nazwisko_niepoprawne($nazwisko){
+    if((strlen($nazwisko)<2) || (strlen($nazwisko)>20) || (ctype_alpha($nazwisko)==false)) 
+    {
+        return true;
+    }
+    return false;
+}
+
+function sprawdz_czy_haslo_rozne($haslo, $haslo2){
+    if($haslo!=$haslo2){
+        return true;
+    }
+    return false;
+}
+
+function sprawdz_czy_haslo_niepoprawne($haslo){
+    if((strlen($haslo)<8) || (strlen($haslo)>20)){
+        return true;
     }
     return false;
 }
