94aa35fce6e6f34587b2d57aa9af3510abeca9d3
Czosnek <martaszablowska21@gmail.com>
2021-02-22 13:50:24 +0100

    pasek nawigacyjny

--- logowanie.php
+++ logowanie.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8" />
     <title>Górskie wędrówki</title>
     <link rel="stylesheet" href="main.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> 
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script> 
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> 
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>  -->
 </head>
 
 <body>
@@ -15,9 +15,9 @@
     ?>
     <?php require_once "menu.php"; ?>
     <div class="container">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#rejestracjaModal">
+        <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#rejestracjaModal">
             Zarejestruj się
-        </button>
+        </button> -->
         <div class="box">
             <form name="logForm" action="zaloguj.php" method="post">
                 <h2>Zaloguj się</h2>
@@ -27,7 +27,7 @@
             </form>
         </div>
 
-        <div class="modal fade" id="rejestracjaModal" >
+        <!-- <div class="modal fade" id="rejestracjaModal" >
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -80,7 +80,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script> -->
 
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -1,18 +1,46 @@
-<div class="menu">
-    <ul>
-        <li><a href="index.php">Strona główna</a></li>
-        <li><a href="wyprawy.php">Nasze wyprawy</a></li>
-        <li><a href="kursy.php">Kursy</a></li>
-        <?php if (isset($_SESSION['id_uzytkownika'])    ) { ?>
-            <li><a href="moje_kursy.php">Moje Kursy</a></li>
-        <?php }  ?>
-        <li><a href="o_nas.php">O nas</a></li>
 
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>menu</title>
+        <link rel="stylesheet" href="main.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+    <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">Górskie wędrówki</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+            <li><a href="index.php">Strona główna</a></li>
+            <li><a href="wyprawy.php">Nasze wyprawy</a></li>
+            <li><a href="kursy.php">Kursy</a></li>
+            <?php if (isset($_SESSION['id_uzytkownika'])    ) { ?> 
+                <li><a href="moje_kursy.php">Moje Kursy</a></li>
+            <?php }  ?>
+            <li><a href="o_nas.php">O nas</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
         <?php if (isset($_SESSION['id_uzytkownika'])    ) { ?>
             <li><a href="wyloguj.php">Wyloguj się</a></li>
         <?php } else { ?>
-            <li><a href="logowanie.php">Zaloguj się</a></li>
+            <li><a href="logowanie.php"><span class="glyphicon glyphicon-log-in"></span>Zaloguj się</a></li>
         <?php } ?>
-
-    </ul>
-</div>
+        </ul>
+    </div>
+    </div>
+    </nav>
+    </body>
+</html>
